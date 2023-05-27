bb3c312ad93a8da84c52b6812334ef01297987a7
KarlTred265 Tred <tred26559@gmail.com>
2023-05-27 16:27:08 +0000

    add the start steps

---
+++ golf/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import './App.css';
+
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <h1>Golf socil media</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ golf/src/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        {/* GolfMedias Icon */}
+        
+        <SidebarOption text="Home"/>
+        <SidebarOption text="Explore"/>
+        <SidebarOption text="Message"/>
+        <SidebarOption text="Categories"/>
+        <SidebarOption text="Profile"/>
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+
+        {/* Button -> Golf posts */}
+    </div>
+  )
+}
+
+export default Sidebar
